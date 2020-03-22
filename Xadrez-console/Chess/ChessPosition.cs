4f6b05ec903362f90a209987988b24fcf47e8cce
Luís <lfilipems@hotmail.com>
2020-03-22 20:58:36 +0000

    Creating ChessPosition class

---
+++ Xadrez-console/Chess/ChessPosition.cs
@@ -0,0 +1,29 @@
+﻿using position;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xadrez_console.Chess
+{
+    class ChessPosition
+    {
+        public char Column { get; set; }
+        public int Row { get; set; }
+
+        public ChessPosition(char column, int row)
+        {
+            Column = column;
+            Row = row;
+        }
+
+        public Position ConvertToPosition() 
+        {
+            return new Position(8 - Row, Column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Column + Row;
+        }
+    }
+}
--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -12,23 +12,10 @@ namespace Xadrez_console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Board board = new Board(8, 8);
-                board.PlacePiece(new Tower(board, Color.Black), new Position(0, 0));
-                board.PlacePiece(new King(board, Color.Black), new Position(1, 3));
-                board.PlacePiece(new King(board, Color.Black), new Position(0, 4));
-                
+            ChessPosition chessPosition = new ChessPosition('c', 7);
 
-                Screen.printBoard(board);
-                Console.ReadLine();
-            }
-            catch (BoardException e)
-            {
-
-                Console.WriteLine(e.Message);
-            }
-            
+            Console.WriteLine(chessPosition.ConvertToPosition());
+            Console.WriteLine(chessPosition);
         }
     }
 }
